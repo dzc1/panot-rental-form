ee388bd450d516ecdbbd07ec11a232d4a8cffd42
Diego Zito <diegozito.m@gmail.com>
2023-09-27 17:27:13 +0200

    testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     stripeSecret: process.env.STRIPE_SK,
     productionSuccessURL: process.env.PRODUCTION_URL,
-    developmentSuccessURL: process.env.DEVELOPMENT,
+    developmentSuccessURL: process.env.DEVELOPMENT_URL,
     public: {
       appUrl: process.env.APP_URL,
       stripePk: process.env.STRIPE_PK,
